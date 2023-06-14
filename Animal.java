87faf724c095b32feff34d1fc403a3f45314fa9d
Jake Pierson <jake.m.pierson@gmail.com>
2023-06-14 07:24:40 -0500

    Final refactoring corrections

--- Animal.java
+++ Animal.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Animal {
+public class Animal {
+    public static void main(String[] args) {
+        factory factory = new factory();
+        AnimalFactory animalFactory1 = factory.get("Kangaroo");
+        animalFactory1.create();
+        AnimalFactory animalFactory2 = factory.get("Cheetah");
+        animalFactory2.create();
+        AnimalFactory animalFactory3 = factory.get("Lion");
+        animalFactory3.create();
+    }
 }
