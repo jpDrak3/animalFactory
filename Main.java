5dddc22c3ee7984960e9d996fbcfa21ba419bf2c
Jake Pierson <jake.m.pierson@gmail.com>
2023-10-02 22:46:40 -0500

    Updated conflicting program name. Renamed Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Animal {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Initialize the Factory class
